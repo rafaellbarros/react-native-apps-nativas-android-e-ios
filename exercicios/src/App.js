fd9307be1258faa7be65f765ddb0749c0536ea96
Rafael Barros <rafaelbarros.df@gmail.com>
2021-08-02 17:52:18 -0300

    :construction: Renderizaçao Condicional

--- exercicios/src/App.js
+++ exercicios/src/App.js
@@ -1,12 +1,14 @@
 import React, {Component} from 'react'
 import {View, Text, StyleSheet} from 'react-native'
+import ParImpar from './components/ParImpar'
 import Simples from './components/Simples'
 
 export default class App extends Component {
 	render() {
 		return (
 			<View style={styles.container}>
-				<Simples texto='Flexível!!!' />
+				<Simples texto='Flexível!1' />
+				<ParImpar numero={32} />
 			</View>
 		)
 	}
