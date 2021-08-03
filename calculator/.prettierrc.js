75152b445fcc9d1d09557db8b1ffb46b1deccbde
Rafael Barros <rafaelbarros.df@gmail.com>
2021-08-03 11:59:26 -0300

    :construction: Criando projeto calculator

---
+++ calculator/.prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  bracketSpacing: false,
+  jsxBracketSameLine: true,
+  singleQuote: true,
+  trailingComma: 'es5',
+  arrowParens: 'avoid',
+  semi: false,
+  jsxSingleQuote: true,
+  tabWidth: 2,
+  useTabs: true,
+  printWidth: 100,
+};
