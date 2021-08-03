0a3cfe73fb16a08aaef9bd2d4c43b6f20fcc8a69
Rafael Barros <rafaelbarros.df@gmail.com>
2021-08-03 12:11:13 -0300

    :construction: Comonente Botao

--- calculator/.prettierrc.js
+++ calculator/.prettierrc.js
@@ -1,12 +1,12 @@
 module.exports = {
-  bracketSpacing: false,
-  jsxBracketSameLine: true,
-  singleQuote: true,
-  trailingComma: 'es5',
-  arrowParens: 'avoid',
-  semi: false,
-  jsxSingleQuote: true,
-  tabWidth: 2,
-  useTabs: true,
-  printWidth: 100,
-};
+	bracketSpacing: false,
+	jsxBracketSameLine: true,
+	singleQuote: true,
+	trailingComma: 'es5',
+	arrowParens: 'always',
+	semi: false,
+	jsxSingleQuote: true,
+	tabWidth: 2,
+	useTabs: true,
+	printWidth: 100,
+}
